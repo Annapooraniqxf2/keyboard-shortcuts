91263ae21d0cf1eb88adba023588dbcc055e053c
Rahul.Bhave <rahul.bhave@qxf2.com>
2020-05-27 22:23:29 +0530

    vs keyboard shortcut exercise

---
+++ vs_keyboard_shortcuts/file1.py
@@ -0,0 +1,12 @@
+"""
+File for keyboard exercise
+"""
+print("Hello, this is you first exercise!")
+print("You can search through this file")
+print("You can search through this file")
+print("You can search through this file")
+print("You can search through this file")
+print("You can search through this file")
+print("You can search through this file")
+print("You can search through this file")
+print("You can search through this file")
