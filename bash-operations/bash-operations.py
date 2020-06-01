ad1fe254b1982bb31d953321ca02c4c4ee287226
Annapoorani Bala <Arunkumar>
2020-06-01 18:43:44 +0530

    Added the exercises for bash operation

---
+++ bash-operations/bash-operations.py
@@ -0,0 +1,18 @@
+"""
+Here we give a set of exercises to practice. All these exercises to be performed by using keyboard. To perform all these exercises we are going to use our Qxf2 Page Object Model reposiotory. So makesure you have already cloned the reposiotory in your local machine.
+"""
+
+1. search through a directory for a file
+    I hope you have already gone through our Page Object Model. So You have to find in which file we have kept all the common selenium methods. If you still didnt get it I can say that it's Base_Page.py. Can you please search the file location without exploring into windows, without using mouse?
+
+2. create a new file without leaving the current directory
+    If I ask you to create a file inside a particular directory. What do you do?
+    If you dont have a practice to use the keyboard we use mouse and we create file.I believe you are inside the qxf2-page-object model directory.If not please change the directory location. Now you have to create a new file called file1.py in the another reposiotory. Make sure you use the keyboard only.
+
+3. copy files from one location to another
+    Maybe you should know the what command you have to use it for copying the files from one reposiotory to another reposiotory without using mouse. The reason here you will be going to use our framewrok and you have to copy some set of files from the qxf2-page-object-model to your own reposiotory for example files like Base_page.py, DriverFactory and some files inside the utils folder. So try copying all the files now.
+
+4. move files from one location to another
+
+
+5. create an alias with variable names to help me move faster
